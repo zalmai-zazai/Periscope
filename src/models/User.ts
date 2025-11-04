d0c30b5cf0295ead7d247cc92f5da355c12d4580
zalmai-zazai <shahzalmai98@gmail.com>
2025-11-04 12:24:01 -0800

    user modal is fixed

--- src/models/User.ts
+++ src/models/User.ts
@@ -102,7 +102,7 @@ UserSchema.pre("save", async function (next) {
 
   try {
     const salt = await bcrypt.genSalt(12);
-    this.password = await bcrypt.hash(this.password, salt);
+    this.password = await bcrypt.hash(this.password as string, salt);
     next();
   } catch (error: any) {
     next(error);
